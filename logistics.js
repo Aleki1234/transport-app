2b28776b83d77b1f501b00ef0432b7cf0c6bbc81
Aleki1234 <chegealex02@gmail.com>
2024-01-09 10:21:44 +0300

    connected

---
+++ logistics.js
@@ -0,0 +1,16 @@
+let Log=document.getElementById('lo1')
+let loginDetails=document.getElementById("for1")
+let loginDetails2=document.getElementById("for2")
+
+function logi(){
+
+loginDetails.style.cssText="color:black; margin-top:20px; text-trasform:uppercase; display:block; font-weight:bold";
+loginDetails2.style.display='none'
+
+}
+function loginTwo(){
+
+    loginDetails2.style.cssText="color:black; margin-top:20px; text-trasform:capitalize; display:block; font-weight:bold";
+    loginDetails.style.display='none'
+    
+    }
